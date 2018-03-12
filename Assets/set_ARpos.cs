3abafaf6a4e0af9b6b54fade70bdcbb2383fb928
aldrich1221 <aldrich1221@gmail.com>
2018-03-12 21:18:34 +0800

    tracker link AR position

---
+++ Assets/set_ARpos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class set_ARpos : MonoBehaviour {
+
+	public GameObject AR_camera;
+	private int framecount = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		framecount++;
+		if (framecount % 2 == 0)
+			AR_camera.transform.position = transform.position;
+	}
+}
