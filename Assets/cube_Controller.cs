e499e2bd5b551cdbb404b96bbac54d13c878abe1
aldrich1221 <aldrich1221@gmail.com>
2018-03-12 12:38:12 +0800

    delete SteamVR, MetaSDK

---
+++ Assets/cube_Controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube_Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate (new Vector3 (0, 1, 0) * Time.deltaTime * 100.0f, Space.World);
+		}
+	}
+}
