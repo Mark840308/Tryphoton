4eb3aac178f24f8cbec9a5be8ea66722529fdda2
aldrich1221 <aldrich1221@gmail.com>
2018-03-12 16:44:59 +0800

    add avatar and photon networkControl

---
+++ Assets/Script/networkHandle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class networkHandle : MonoBehaviour {
+
+	public GameObject currentCamera;
+	private GameObject currentPlayer;
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.logLevel = PhotonLogLevel.Full;
+		PhotonNetwork.ConnectUsingSettings ("0.1");
+	}
+	
+	public void OnJoinedLobby()
+	{
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	public void OnJoinedRoom()
+	{
+		currentPlayer = PhotonNetwork.Instantiate("Avatar", currentCamera.transform.position, currentCamera.transform.rotation, 0);
+	}
+
+	void OnPhotonRandomJoinFailed()
+	{
+		Debug.Log("Can't join random room!");
+		PhotonNetwork.CreateRoom(null);
+	}
+
+	void OnGUI()
+	{
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+
+}
